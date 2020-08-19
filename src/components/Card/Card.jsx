bf9f7a9c632fde1222f3c5507745f80cca6aca98
Evgenii Popov <axiom777@yandex.ru>
2020-08-19 21:09:51 +0300

    Add styles and code for Active

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./card.scss";
 import num2str from "../../helpers/num-to-string.js";
 import catPhoto from "../../images/Photo.png";
+
 export default function Card(props) {
-  console.log(props.data);
   const data = props.data;
-  const { status, heading, name, composition, unit, desription } = data;
+  const { heading, name, composition, unit, desription } = data;
+  const defultStatus = data.status;
   const weight = parseFloat(data.weight).toLocaleString("ru-RU");
   const portions = parseInt(data.portions).toLocaleString("ru-RU");
   const mouses = parseInt(data["mouse-count"]);
@@ -14,26 +15,87 @@ export default function Card(props) {
   const desriptionLinkText = data["desription-link"];
   const selectedDesctiprion = data["selected-desription"];
 
+  const [status, setStatus] = useState(defultStatus);
+  const [isHovered, setIsHovered] = useState(false);
+
   function getOffer() {
     const mouse = num2str(mouses, wordForms);
-    const str =
-      mouses === 1 ? (
-        mouse
-      ) : (
+    let str;
+    if (mouses === 1) {
+      str = mouse;
+    } else {
+      str = (
         <>
-          <strong>{mouses}</strong> {mouse}
+          {" "}
+          <strong>{mouses}</strong> {mouse}{" "}
         </>
       );
+    }
     return (
       <>
         <strong>{portions}</strong> порций {str}
       </>
     );
   }
+
+  function getCardClass() {
+    const classes = [];
+    classes.push("card");
+    status === "active" && classes.push("card_active");
+    status === "disabled" && classes.push("card_disabled");
+    return classes.join(" ");
+  }
+
+  function toggleStatus() {
+    switch (status) {
+      case "default":
+        setStatus("active");
+        break;
+      case "active":
+        setStatus("default");
+        break;
+      default:
+        break;
+    }
+  }
+
+  function getDesctiption() {
+    switch (status) {
+      case "active":
+        return selectedDesctiprion;
+      case "disabled":
+        return <>Печалька {composition} закончился.</>;
+      default:
+        return (
+          <>
+            {" "}
+            {desription}{" "}
+            <span className="card__link">{desriptionLinkText}.</span>{" "}
+          </>
+        );
+    }
+  }
+
+  function getHeadingClasses() {
+    const classes = [];
+    classes.push("card__heading");
+    status === "active" && isHovered && classes.push("card__heading_hovered");
+    return classes.join(" ");
+  }
+
+  function getHeading() {
+    return isHovered && status === "active" ? headingHovered : heading;
+  }
+
   return (
-    <div className="card">
+    <div
+      className={getCardClass()}
+      onClick={() => toggleStatus()}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="card__block">
-        <div className="card__heading">{heading}</div>
+        <div className={getHeadingClasses()}>{getHeading()}</div>
         <div className="card__name">{name}</div>
         <div className="card__composition">{composition}</div>
         <div className="card__offer">{getOffer()} в подарок</div>
@@ -41,13 +103,11 @@ export default function Card(props) {
           <img className="card__image" src={catPhoto} alt="cat" />
         </div>
         <div className="card__weight">
-            <div className="card__weight-value">{weight}</div>
-            <div className="card__unit">{unit}</div>
+          <div className="card__weight-value">{weight}</div>
+          <div className="card__unit">{unit}</div>
         </div>
       </div>
-      <div className="card__description">
-        {desription} <span className="card__link">{desriptionLinkText}.</span>
-      </div>
+      <div className="card__description">{getDesctiption()}</div>
     </div>
   );
 }
